66effdc66931270504f5afd71d57bbc4022cfde8
OBrooks <mr.willh@gmail.com>
2019-06-14 18:10:13 +0200

    updated Gemfile + assets.rb with .woff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += [/.*\.png/,/.*\.ico/,/.*\.jpg/,/.*\.js/,/.*\.scss/,/.*\.svg/,/.*\.woff/]
-Rails.application.config.assets.precompile << %w( *.scss *.js )
+Rails.application.config.assets.precompile << %w( *.scss *.js *.woff)
 Rails.application.config.assets.paths << Rails.root.join("lib")
 Rails.application.config.assets.paths << Rails.root.join("vendor")
 # Precompile additional assets.
