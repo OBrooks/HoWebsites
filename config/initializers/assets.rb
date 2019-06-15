9b54af019071d01d22e8a06eb6960ae89b6788e2
OBrooks <mr.willh@gmail.com>
2019-06-15 12:12:33 +0200

    assets.rb modified + added again Uglifier

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,4 @@ Rails.application.config.assets.precompile << %w( *.scss *.js *.woff)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( admin.js admin.css )
