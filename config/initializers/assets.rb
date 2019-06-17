ff9ea660f015c2c6d71f11f74fb1c41d69bbaaa2
OBrooks <mr.willh@gmail.com>
2019-06-17 10:16:54 +0200

    upgraded to Rails 6

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("lib")
 Rails.application.config.assets.paths << Rails.root.join("vendor")
 Rails.application.config.assets.precompile += [/.*\.png/,/.*\.ico/,/.*\.jpg/,/.*\.js/,/.*\.scss/,/.*\.svg/,/.*\.woff/]
-Rails.application.config.assets.precompile << %w( *.scss *.js *.woff)
+Rails.application.config.assets.precompile << %w( *.scss *.js )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
