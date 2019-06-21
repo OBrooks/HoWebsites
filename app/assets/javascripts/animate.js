aa0bda8050f337bf56705ffdd896db51cbb5fc1e
OBrooks <mr.willh@gmail.com>
2019-06-21 09:57:43 +0200

    try with new js animation

--- app/assets/javascripts/animate.js
+++ app/assets/javascripts/animate.js
@@ -1,30 +1,113 @@
 document.addEventListener("turbolinks:load", function () {
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function fitElementToParent(el, padding) {
+    var timeout = null;
+
+    function resize() {
+        if (timeout) clearTimeout(timeout);
+        anime.set(el, {
+            scale: 1
+        });
+        var pad = padding || 0;
+        var parentEl = el.parentNode;
+        var elOffsetWidth = el.offsetWidth - pad;
+        var parentOffsetWidth = parentEl.offsetWidth;
+        var ratio = parentOffsetWidth / elOffsetWidth;
+        timeout = setTimeout(anime.set(el, {
+            scale: ratio
+        }), 10);
+    }
+    resize();
+    window.addEventListener('resize', resize);
 }
 
-    anime({
-    targets: 'svg.small-circle',
-    keyframes: [{
-            translateY: getRandomIntInclusive(- 300, 1200 )
-        },
-        {
-            translateX: getRandomIntInclusive(-300, 1200)
-        },
-        {
-            translateY: getRandomIntInclusive(-300, 1200)
-        },
-        {
-            translateX: 0
-        },
-        {
-            translateY: 0
+function layeredAnimation() {
+
+    // var transformEls = document.querySelectorAll('.transform-progress');
+    var layeredAnimationEl = document.querySelector('.animation-background');
+    var shapeEls = layeredAnimationEl.querySelectorAll('.shape');
+    // var triangleEl = layeredAnimationEl.querySelector('polygon');
+    // var trianglePoints = triangleEl.getAttribute('points').split(' ');
+    var easings = ['easeInOutQuad', 'easeInOutCirc', 'easeInOutSine', 'spring'];
+
+    fitElementToParent(layeredAnimationEl);
+
+    function createKeyframes(value) {
+        var keyframes = [];
+        for (var i = 0; i < 30; i++) keyframes.push({
+            value: value
+        });
+        return keyframes;
+    }
+
+    function animateShape(el) {
+
+        var circleEl = el.querySelector('circle');
+        // var rectEl = el.querySelector('rect');
+        // var polyEl = el.querySelector('polygon');
+
+        var animation = anime.timeline({
+                targets: el,
+                duration: function () {
+                    return anime.random(600, 2200);
+                },
+                easing: function () {
+                    return easings[anime.random(0, easings.length - 1)];
+                },
+                complete: function (anim) {
+                    animateShape(anim.animatables[0].target);
+                },
+            })
+            .add({
+                translateX: createKeyframes(function (el) {
+                    return el.classList.contains('large') ? anime.random(-300, 300) : anime.random(-520, 520);
+                }),
+                translateY: createKeyframes(function (el) {
+                    return el.classList.contains('large') ? anime.random(-110, 110) : anime.random(-280, 280);
+                }),
+                rotate: createKeyframes(function () {
+                    return anime.random(-180, 180);
+                }),
+            }, 0);
+        if (circleEl) {
+            animation.add({
+                targets: circleEl,
+                r: createKeyframes(function () {
+                    return anime.random(32, 72);
+                }),
+            }, 0);
         }
-    ],
-    duration: 10000,
-    easing: 'easeOutElastic(1, .8)',
-    loop: true
-});
+        // if (rectEl) {
+        //     animation.add({
+        //         targets: rectEl,
+        //         width: createKeyframes(function () {
+        //             return anime.random(64, 120);
+        //         }),
+        //         height: createKeyframes(function () {
+        //             return anime.random(64, 120);
+        //         }),
+        //     }, 0);
+        // }
+        // if (polyEl) {
+        //     animation.add({
+        //         targets: polyEl,
+        //         points: createKeyframes(function () {
+        //             var scale = anime.random(72, 180) / 100;
+        //             return trianglePoints.map(function (p) {
+        //                 return p * scale;
+        //             }).join(' ');
+        //         }),
+        //     }, 0);
+        // }
+
+    }
+
+    for (var i = 0; i < shapeEls.length; i++) {
+        animateShape(shapeEls[i]);
+    }
+
+};
+console.log("going to begin")
+layeredAnimation()
+console.log("done")
 });
