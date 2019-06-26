6afe8c2986294223e705265a05a5bd27b8e1320b
OBrooks <mr.willh@gmail.com>
2019-06-26 17:39:15 +0200

    added languages & projects, converted 'em' in 'pt'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get "about", to: "home#about"
+    get "about",              to: "home#about"
+    get "projects",           to: "home#projects"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
