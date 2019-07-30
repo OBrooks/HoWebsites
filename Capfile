3f0d8e23b55f815de4a370550941539a5f402497
OBrooks <mr.willh@gmail.com>
2019-07-30 12:17:42 +0200

    replaced 'capistrano/rails' with 'capistrano/bundler' & 'capistrano/rails'

--- Capfile
+++ Capfile
@@ -28,7 +28,9 @@ install_plugin Capistrano::SCM::Git
 #
 # require "capistrano/rvm"
 require "capistrano/rbenv"
-require "capistrano/rails"
+require 'capistrano/bundler' 
+require 'capistrano/rails/assets'
+# require "capistrano/rails"
 # require "capistrano/chruby"
 # require "capistrano/bundler"
 # require "capistrano/rails/assets"
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:OBrooks/my_website_db.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/brooks/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
