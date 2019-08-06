d34d13489a196f54a9193577563b8a70897cb83d
OBrooks <mr.willh@gmail.com>
2019-08-06 13:49:17 +0200

    changed 'my_website_db' to 'HoWebsites'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_website_db"
-set :repo_url, "git@github.com:OBrooks/my_website_db.git"
+set :application, "HoWebsites"
+set :repo_url, "git@github.com:OBrooks/HoWebsites.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
